261de9003b5005260dfee18535819b6e85540c88
Abdo Elbessa <zezoelbessa10@egyeagles.com>
2024-01-08 20:32:43 +0200

    Add project files.

---
+++ Vistor/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Vistor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VisitorTracker tracker1 = VisitorTracker.GetInstance();
+            VisitorTracker tracker2 = VisitorTracker.GetInstance();
+            VisitorTracker tracker3 = VisitorTracker.GetInstance();
+
+            int visitorCount = tracker1.GetVisitorCount();
+            Console.WriteLine($"Number of visitors: {visitorCount}");
+
+            if (tracker1 == tracker2 && tracker2 == tracker3)
+            {
+                Console.WriteLine("Singleton Works");
+            }
+            else
+            {
+                Console.WriteLine("Oops! Singleton doesn't work");
+            }
+
+        }
+    }
+}
---
+++ Vistor/VisitorTracker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vistor
+{
+    public class VisitorTracker
+    {
+        private static VisitorTracker _vistor;
+        private static int visitorCount;
+
+        private VisitorTracker()
+        {
+            visitorCount = 0;
+        }
+
+        public static VisitorTracker GetInstance()
+        {
+         
+                if (_vistor == null)
+                {
+                    _vistor = new VisitorTracker();
+                }
+                visitorCount++;
+                return _vistor;
+        }
+
+        public int GetVisitorCount()
+        {
+            return visitorCount;
+        }
+    }
+
+}
